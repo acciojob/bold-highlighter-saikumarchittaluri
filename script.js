5460de54cc477a7cc355c181f3eb8db934c85689
saikumarchittaluri <127985962+saikumarchittaluri@users.noreply.github.com>
2023-08-22 15:41:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-function highlight() {
-    //Write your code here
+ function highlight() {
+  // Get all the bold elements within the paragraph
+  const boldWords = document.querySelectorAll('p strong');
 
-
+  // Change the color of bold words to green
+  boldWords.forEach(word => {
+    word.style.color = 'green';
+  });
 }
 
-
 function return_normal() {
-    //Write your code here
+  // Get all the bold elements within the paragraph
+  const boldWords = document.querySelectorAll('p strong');
 
-    
+  // Revert the color of bold words back to black
+  boldWords.forEach(word => {
+    word.style.color = 'black';
+  });
 }
